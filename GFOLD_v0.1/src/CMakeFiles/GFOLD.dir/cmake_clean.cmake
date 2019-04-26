792f97490b2c8647dd23141776fc0f27e089d863
Hou <jh7x3@multicom.eecs.missouri.edu>
2019-04-26 15:17:24 -0500

    Update GFOLD development environment

---
+++ GFOLD_v0.1/src/CMakeFiles/GFOLD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/GFOLD.dir/utils/LogFactorial.cpp.o"
+  "../libs/libGFOLD.pdb"
+  "../libs/libGFOLD.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/GFOLD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
