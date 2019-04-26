6a5d9b73dce5b05e22700788e1c1513d8e8ea2da
Hou <jh7x3@multicom.eecs.missouri.edu>
2019-04-26 15:32:38 -0500

    Update scripts

---
+++ GFOLD_v0.1/examples/CMakeFiles/simple.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/data/jh7x3/GFOLD_v0.1/examples/simple.cpp" "/data/jh7x3/GFOLD_v0.1/examples/CMakeFiles/simple.dir/simple.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/data/jh7x3/GFOLD_v0.1/src/CMakeFiles/GFOLD.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "/data/commons/tools/boost_1_55_0/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
